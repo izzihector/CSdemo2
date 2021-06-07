5634e823dd87086851e0e06db47ef6d3f29149a9
Bharat <bharat@caretit.com>
2021-06-07 22:20:33 +0530

    add modules

---
+++ Aardug_Show_Followers/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Part of Aardug. (Website: www.aardug.nl).
+# See LICENSE file for full copyright and licensing details.
+#
+##############################################################################
+
+{
+    'name' : 'Show Mail Followers',
+    'version': '14.0.0.9',
+    'summary': 'show Mail Followers in mail composer form',
+    'category': 'products',
+    'description': """ This Module Allowes to you See Followers on Mail Form""",
+    'author': 'Aardug',
+    'website': 'http://www.aardug.nl/',
+    'support': 'info@aardug.nl',
+    'depends': ['mail'],
+    'data' : [
+            'wizard/mail_compose_message_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ custom_demo_data/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name":"Custom Demo Data Addon",
+    'depends': ['product',"crm"],
+    "data": [
+        "data/data_contacts.xml",
+        "data/data_product.xml",
+        "data/data_crm.xml",
+    ],
+    "installable": True,
+}
